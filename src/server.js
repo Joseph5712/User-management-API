b3365ed51a5b6a43a17318f6de1f2c10a444f152
Joseph5712 <jmm7gameover@gmail.com>
2025-12-25 15:58:20 -0600

    Add MongoDB connection and User model

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
 const app = require("./app");
 require("dotenv").config();
 
+const { connectDB } = require("./config/db");
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+
+
+async function start() {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`✅ Server running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        process.exit(1);
+    }   
+}
+
+start();
+
+
